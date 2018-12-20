6ef8771bc6ae6689d1e189db477a9d34b57f09bd
Natalie Halawani <hala1224@gmail.com>
2018-12-20 16:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function handlebarsSetup() {
     return new Handlebars.SafeString('<li name="ingredients">' + ingredient + '</li>');
   });
   // Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-detail-partial").innerHTML);
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 }
 
